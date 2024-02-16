473547f667e5721801caad8799010be269bdb68a
TiyNa <69409185+TiyNa@users.noreply.github.com>
2024-02-17 00:04:16 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def xmSport():
+    body = {"status": False, "code": "请检查手机号码和登录密码"}
+    
     url = f"https://api-user.huami.com/registrations/+86{request.form.get('phoneNumber')}/tokens"
     headers = {
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
@@ -19,20 +21,16 @@ def xmSport():
         "redirect_uri": "https://s3-us-west-2.amazonaws.com/hm-registration/successsignin.html",
         "token": "access"
     }
+    
     response = requests.post(url, headers=headers, data=data, allow_redirects=False)
-    body = {"status": False}
 
     try:
-        print(response.headers)
-        location = response.headers["Location"]
-        match = re.search("(?<=access=).*?(?=&)", location)
+        match = re.search("(?<=access=).*?(?=&)", response.headers["Location"])
         if match:
             body['status'] = True
             body['code'] = match.group()
-        else:
-            body['code'] = "请检查手机号码和登录密码"
-    except IndexError:
-            body['code'] = "请求失败，请稍后重试"
+    except:
+        body['code'] = "请求失败，请稍后重试"
 
     return jsonify(body)
 
